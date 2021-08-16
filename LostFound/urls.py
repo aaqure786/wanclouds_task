72abd01a61f31d3e1c04990c8997acba4e4664ee
= <=abrarqureshi8998@gmail.com>
2021-08-16 11:17:10 +0500

    Adding Crud Function of Found Items and setting urls

--- LostFound/urls.py
+++ LostFound/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import UserRegAPI,LostItemAPI,LostItemDetailAPI
+from .views import UserRegAPI,LostItemAPI,LostItemDetailAPI,FoundItemAPI,FoundItemDetailAPI
 
 urlpatterns = [
-    path('user_registration',UserRegAPI.as_view(), name="user_registration"),
-    path('lost_item',LostItemAPI.as_view(), name="lost_item"),
-    path('lost_item_detail',LostItemDetailAPI.as_view(),name="lost_item_detail")
+    path('user_registration/',UserRegAPI.as_view(), name="user_registration"),
+    path('lost_item/',LostItemAPI.as_view(), name="lost_item"),
+    path('lost_item_detail/<int:id>/',LostItemDetailAPI.as_view(),name="lost_item_detail"),
+    path('found_item/',FoundItemAPI.as_view(), name="found_item"),
+    path('found_item_detail/<int:id>/',FoundItemDetailAPI.as_view(),name="found_item_detail"),
 ]
--- LostFound/views.py
+++ LostFound/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from rest_framework import serializers
 
 from rest_framework.decorators import api_view
-from LostFound.serializers import UserRegSerializer ,LostItemSerializer
+from LostFound.serializers import UserRegSerializer ,LostItemSerializer , FoundItemSerializer
 from django.http import HttpRequest, JsonResponse
-from .models import Lost_Item, UserRegistration 
+from .models import Lost_Item, UserRegistration,Found_Item
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication ,BasicAuthentication
@@ -61,6 +61,45 @@ class LostItemDetailAPI(APIView):
         item= self.get_objects(id=id)
         item.delete()
         return Response(status = status.HTTP_404_NOT_FOUND)
+
+
+class FoundItemAPI(APIView):
+    def get(self, request):
+        lost_item  = Found_Item.objects.all()
+        serializers =FoundItemSerializer(lost_item, many=True)
+        return Response(serializers.data)
+    def post(self,request):
+        serializers = FoundItemSerializer(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class FoundItemDetailAPI(APIView):
+    def get_objects(self,id):
+        try:
+            return Found_Item.objects.get(id= id )
+        except Found_Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self,request,id):
+        item = self.get_objects(id=id)
+        serializers = FoundItemSerializer(item)
+        return Response(serializers.data)
+
+    def put(self,request,id):
+        item= self.get_objects(id=id)
+        serializers=FoundItemSerializer(item, data= request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors,status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self,request,id):
+        item= self.get_objects(id=id)
+        item.delete()
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
 
 
 
